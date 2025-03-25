4dc787804cf63c30f6db906f502565ec792b43f7
Jose David Lagos Morales <mldesojose@gmail.com>
2025-03-25 01:52:57 -0500

    fea:login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,37 +1,27 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // <-- Asegúrate de importar esto
-import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,HttpClientModule], // <-- Importa FormsModule aquí
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  credentials = {
-    username: '',
-    password: ''
-  };
+  userName: string = '';
+  password: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private router: Router) {}
 
-  onSubmit() {
-    
-    this.http.post('http://localhost:3000/api/Usuario/login', this.credentials)
-      .subscribe(
-        (response: any) => {
-          // Guardar el token en el almacenamiento local
-          localStorage.setItem('token', response.token);
-          // Redirigir al usuario a la página de inicio
-          this.router.navigate(['/home']);
-        },
-        (error) => {
-          console.error('Error en el inicio de sesión:', error);
-          alert('Credenciales incorrectas');
-        }
-      );
+  onLogin() {
+    // Lógica de autenticación (puedes integrar AuthService aquí)    
+    this.router.navigate(['/main']); // Redirige tras login exitoso
+  }
+
+  goBack() {
+    this.router.navigate(['/']); // Navega a la página anterior
   }
 }
